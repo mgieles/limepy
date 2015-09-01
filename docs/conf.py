b0aef9081c3d48a7861f0078c5e22353356159aa
Eduardo Balbinot <eduardo.balbinot@gmail.com>
2015-09-01 09:00:04 +0100

    test conf for docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.integrate', 'scipy.interpolate',
-                'scipy.special', 'scipy.misc', 'optimize', 'random']
+                'scipy.special', 'scipy.misc', 'optimize', 'linalg']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 print sys.path
 # If extensions (or modules to document with autodoc) are in another
